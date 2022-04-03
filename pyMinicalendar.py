15abf632a4a6c94010f2806f381323c1bcaa59d6
alfredo pezzotti <alfredo.pezzotti@yahoo.com>
2022-04-03 21:57:31 +0200

    computes the correct position for the root window

--- pyMinicalendar.py
+++ pyMinicalendar.py
@@ -39,49 +39,62 @@ class ScreenProperties:
 
 """ GUI section: """
 
+windowSize_X = 250
+windowSize_Y = 200
+
 # Gets the current ABSOLUTE mouse coordinates:
 cursorPos = MousePointerPos(0, 0)
 cursorPos.x, cursorPos.y = pyag.position()
 # DEBUG:
-print(str(cursorPos.x) + ", " + str(cursorPos.y))
+print("1 " + str(cursorPos.x) + ", " + str(cursorPos.y))
 
 # gets the current screen resolution:
 thisScreen = ScreenProperties(0, 0)
 thisScreen.width, thisScreen.height = pyag.size()
 # DEBUG:
-print(str(thisScreen.width) + ", " + str(thisScreen.height))
+print("2 "+ str(thisScreen.width) + ", " + str(thisScreen.height))
 
 # computes how much the cursor is distant from the screen border.
 # In most cases the clock is in the lower right corner, so the root
 # needs to be moved to the left by a certain amount. In other cases
 # this is not needed (e.g.: when the clock is in the upper right corner).
-cursorDistance_X = cursorPos.x - thisScreen.width
-cursorDistance_Y = cursorPos.y - thisScreen.height
+cursorDistance_X = abs(cursorPos.x - thisScreen.width)
+cursorDistance_Y = abs(cursorPos.y - thisScreen.height)
 # DEBUG:
-print(str(cursorDistance_X) + ", " + str(cursorDistance_Y))
+print("3 " + str(cursorDistance_X) + ", " + str(cursorDistance_Y))
 
 # create a root window
 root = tk.Tk()
 root.wm_title("MiniCalendar")
-root.geometry("250x200")
+root.geometry(str(windowSize_X) + "x" + str(windowSize_Y))
+# sets the window position:
+offset_Y = 40
+if cursorDistance_X < windowSize_X:
+    windowPos_X = cursorPos.x - cursorDistance_X - (windowSize_X - cursorDistance_X)
+else:
+    windowPos_X = cursorPos.x
+if cursorDistance_Y < windowSize_Y:
+    windowPos_Y = cursorPos.y - cursorDistance_Y - (windowSize_Y + offset_Y - cursorDistance_Y)
+else:
+    windowPos_Y = cursorPos.y + offset_Y
+#   window.setXpos(cursorPos.x - dist.x) # if dist.x < 250px
+#   window.setYpos(cursorPos.y - dist.y - 200px) # if dist.y < 200px
+# DEBUG:
+print("4 " + str(windowPos_X) + ", " + str(windowPos_Y))
+
+root.geometry("+" + str(windowPos_X) + "+" + str(windowPos_Y))
+# adds a frame
 frame = tk.Frame(root)
 root.config(bg="#000000")
 frame.pack()
+# adds the top frame. It will contain the Month and Year items:
+topFrame = tk.Frame(root)
+topFrame.pack(side="top")
+#adds the bottom frame, it will contain the Dayz items:
+bottomFrame = tk.Frame(root)
+bottomFrame.pack(side="bottom")
 
-#from tkinter import * 
-# TkForm = Tk()
-#
-#  #Set Height and Width of Window
-#   TkForm.geometry("350x150") 
-#
-#    #Set the Title according to desire
-#     TkForm.title("TK Size in Python")
-#
-#      TkForm.mainloop()
 
-#   window.setXpos(cursorPos.x - dist.x) # if dist.x < 250px
-#   window.setYpos(cursorPos.y - dist.y - 200px) # if dist.y < 200px
-#   spawnWindowRootAtPos();
 #   add widgets();
 #
 # LOGIC section:
